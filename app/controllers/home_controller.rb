54d11a619ad9c347264b124883c94a9a44e2ce22
alikahwaji <ali.kahwaji@hotmail.com>
2017-11-28 16:31:42 +0000

    Creating a form and fetching the data for each company symbol

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,18 @@
 class HomeController < ApplicationController
   def index
-    @stock = StockQuote::Stock.quote("goog")
+    if params[:id] == ""
+      @nothing = "Please enter a valid symbol!"
+      elsif 
+        if params[:id]
+          begin
+               @stock = StockQuote::Stock.quote(params[:id])
+          rescue StandardError
+            @error = "Stock Symbol Doesn't Exist...Please enter a valid symbol!"
+          end
+        end
   end
+end
+
   
   def about 
   end
